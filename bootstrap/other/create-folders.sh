9a617ab816977be61763a8541e02f4e0b6d0e0f6
Matthew Davis <matt@mattdavis.co.uk>
2020-08-15 23:24:51 +0100

    Add missing icon for folder

--- bootstrap/other/create-folders.sh
+++ bootstrap/other/create-folders.sh
@@ -12,8 +12,9 @@ DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p ~/Code
 $DIRECTORY/../../utilities/set-icon.applescript "/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/DeveloperFolderIcon.icns" ~/Code >& /dev/null
 
-# -- ~/Code/Other folder
+# -- ~/Code/Other folder with icon
 mkdir -p ~/Code/Other
+$DIRECTORY/../../utilities/set-icon.applescript "/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/UtilitiesFolder.icns" ~/Code/Other >& /dev/null
 
 # -- ~/Code/Sites folder with icon
 mkdir -p ~/Code/Sites
