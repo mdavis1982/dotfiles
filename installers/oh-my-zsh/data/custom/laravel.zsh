313b7384e028f9987544633ddddcf12d14021eb7
Matthew Davis <matt@mattdavis.co.uk>
2026-06-04 19:47:30 +0100

    Add composer test to aliases.

--- installers/oh-my-zsh/data/custom/laravel.zsh
+++ installers/oh-my-zsh/data/custom/laravel.zsh
@@ -6,3 +6,6 @@ alias tinker="php artisan tinker"
 
 # Tail the Laravel log file, highlighting timestamped entries and exception chains
 alias llog='tail -f -n 450 storage/logs/laravel*.log | grep -i -E "^\[\d{4}\-\d{2}\-\d{2} \d{2}:\d{2}:\d{2}\]|Next [\w\W]+?\:" --color'
+
+# Run composer test
+alias ct='composer test'
